ca64ce8de45aaff0c1cb8710b1be86edfe38b6a6
Nathalia Papst <nathaliapapst@gmail.com>
2022-03-08 16:08:53 -0300

    Adicionar cenário na GameScene

--- FlyGame/FlyGame Shared/Views/GameScene.swift
+++ FlyGame/FlyGame Shared/Views/GameScene.swift
@@ -9,6 +9,13 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    lazy var scenarioImage: SKSpriteNode = {
+        var scenario = SKSpriteNode()
+        scenario = SKSpriteNode(imageNamed: "Cenário")
+        
+        return scenario
+    }()
+    
     class func newGameScene() -> GameScene {
         let scene = GameScene()
         scene.scaleMode = .resizeFill
@@ -16,7 +23,13 @@ class GameScene: SKScene {
     }
     
     func setUpScene() {
+        removeAllChildren()
+        removeAllActions()
         
+        self.addChild(scenarioImage)
+        scenarioImage.size.width = self.size.width
+        scenarioImage.size.height = self.size.height
+        scenarioImage.position = CGPoint(x: scenarioImage.size.width/2, y: scenarioImage.size.height/2)
     }
     
     override func didMove(to view: SKView) {
@@ -24,7 +37,7 @@ class GameScene: SKScene {
     }
     
     override func didChangeSize(_ oldSize: CGSize) {
-        
+        self.setUpScene()
     }
        
     override func update(_ currentTime: TimeInterval) {
