4fc47b0ae8a796826f90007f6467bd6eda0f368a
Gabriele Namie <gabrielenamie@hotmail.com>
2022-03-11 15:09:49 -0300

    arrumando erros

--- FlyGame/FlyGame Shared/Views/MenuScene.swift
+++ FlyGame/FlyGame Shared/Views/MenuScene.swift
@@ -12,25 +12,65 @@ class MenuScene: SKScene {
     
     lazy var scenarioImage: SKSpriteNode = {
         var scenario = SKSpriteNode()
-        scenario = SKSpriteNode(imageNamed: "cenario")
-        
+        scenario = SKSpriteNode(imageNamed: "Scenario")
         return scenario
+    }()
+    
+    lazy var pianoImage: SKSpriteNode = {
+        var piano = SKSpriteNode(imageNamed: "Piano")
+        return piano
+    }()
+    
+    lazy var playButton: SKButtonNode = {
+        var bt = SKButtonNode(image: SKSpriteNode(imageNamed: "PlayButton")) {
+            // TODO: levar implementação para a controller
+            let scene = GameScene.newGameScene()
+            self.view?.presentScene(scene)
+        }
+        return bt
+    }()
+    
+    lazy var soundButton: SKButtonNode = {
+        var bt = SKButtonNode(image: SKSpriteNode(imageNamed: "SoundButton")) {
+        }
+        return bt
+    }()
+    
+    lazy var musicButton: SKButtonNode = {
+        var bt = SKButtonNode(image: SKSpriteNode(imageNamed: "MusicButton")) {
+        }
+        return bt
+    }()
+    
+    lazy var gameCenterButton: SKButtonNode = {
+        var bt = SKButtonNode(image: SKSpriteNode(imageNamed: "GameCenterButton")) {
+        }
+        return bt
+    }()
+    
+    lazy var scoreLabel: SKLabelNode = {
+        var lbl = SKLabelNode()
+        lbl.numberOfLines = 0
+        lbl.fontColor = SKColor.black
+        lbl.fontName = "munro"
+        lbl.text = "Highscore: 2067"
+        return lbl
     }()
     
     class func newGameScene() -> MenuScene {
         let scene = MenuScene()
-        scene.scaleMode = .aspectFill
+        scene.scaleMode = .resizeFill
         return scene
     }
     
     func setUpScene() {
-        removeAllChildren()
-        removeAllActions()
-        
+        self.addChild(scoreLabel)
+        self.addChild(musicButton)
         self.addChild(scenarioImage)
-        scenarioImage.size.width = self.size.width
-        scenarioImage.size.height = self.size.height
-        scenarioImage.position = CGPoint(x: scenarioImage.size.width/2, y: scenarioImage.size.height/2)
+        self.addChild(playButton)
+        self.addChild(soundButton)
+        self.addChild(gameCenterButton)
+        self.addChild(pianoImage)
     }
     
     override func didMove(to view: SKView) {
@@ -38,29 +78,69 @@ class MenuScene: SKScene {
     }
     
     override func didChangeSize(_ oldSize: CGSize) {
-        self.setUpScene()
+        setupNodesPosition()
+        setupNodesSize()
     }
-       
+    
+    private func setupNodesSize() {
+        scenarioImage.size.width = self.size.width
+        scenarioImage.size.height = self.size.height
+        playButton.setScale(self.size.height/410)
+        musicButton.setScale(self.size.height/460.6)
+        soundButton.setScale(self.size.height/460.6)
+        gameCenterButton.setScale(self.size.height/460.6)
+        pianoImage.setScale(self.size.height/400)
+    }
+    
+    private func setupNodesPosition() {
+        scenarioImage.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        scenarioImage.zPosition = 0
+        soundButton.position = CGPoint(x: self.size.width/2, y: self.size.height/3.5)
+        soundButton.zPosition = 1
+        scoreLabel.position = CGPoint(x: self.size.width/2, y: self.size.height/2.5)
+        scoreLabel.zPosition = 1
+        scoreLabel.fontSize = self.size.height/15
+        musicButton.position = CGPoint(x: soundButton.position.x + self.size.width/9.5, y: self.size.height/3.5)
+        musicButton.zPosition = 1
+        gameCenterButton.position = CGPoint(x: soundButton.position.x - self.size.width/9.5, y: self.size.height/3.5)
+        gameCenterButton.zPosition = 1
+        playButton.position = CGPoint(x: self.size.width/2, y: self.size.height/1.6)
+        playButton.zPosition = 1
+        pianoImage.position = CGPoint(x: self.size.width/4, y: self.size.height/2)
+        pianoImage.zPosition = 1
+    }
+    
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
 }
 
 extension MenuScene: MenuLogicDelegate {
-    
     func goToGameCenter() {
-        
     }
-    
     func playGame() {
-        
     }
-    
     func toggleSound() {
-        
     }
-    
     func toggleMusic() {
-        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
