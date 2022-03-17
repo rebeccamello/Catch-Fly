33bb32ab8f9bf93c15e12385c8dddc95843d4c85
Rebecca Mello <rebecca.mello@me.com>
2022-03-17 17:14:21 -0300

    GameOver Scene feita

--- FlyGame/FlyGame Shared/Views/GameScene.swift
+++ FlyGame/FlyGame Shared/Views/GameScene.swift
@@ -135,7 +135,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         pauseMenu.position = CGPoint(x: size.width/2, y: size.height/2)
         pauseButton.position = CGPoint(x: size.width*0.06, y: size.height*0.88)
         scoreLabel.position = CGPoint(x: pauseButton.position.x + scoreLabel.frame.size.width/2 + 50, y: pauseButton.position.y - scoreLabel.frame.size.height/2)
-        print(pauseButton.frame.size.height)
         pauseButton.setScale(self.size.height*0.00035)
     }
     
